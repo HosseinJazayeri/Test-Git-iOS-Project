832f89a4c4c027b5b40d054338d410e3f9e61b5d
AJ <ma@ajs-macbook-pro.home>
2014-07-02 08:23:25 -0400

    Initial Commit

---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by AJ on 7/2/14.
+//  Copyright (c) 2014 Code Coalation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
