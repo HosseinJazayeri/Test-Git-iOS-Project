7ac302f9407b2a6863b5932d619e1d0c2195b5ee
AJ <ma@ajs-macbook-pro.home>
2014-07-06 03:47:14 -0400

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by AJ on 7/6/14.
+//  Copyright (c) 2014 Code Coalation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
